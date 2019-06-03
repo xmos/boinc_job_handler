27adc50cd6c5409eb057aa3de38e4f5e6a2e31ce
Shuchita Khare <shuchitak@Shuchita.local>
2019-06-03 10:07:58 +0100

    commented out usage of actual authorisation files

--- examples_new/test_app_create.py
+++ examples_new/test_app_create.py
@@ -5,8 +5,8 @@ filepath = os.path.dirname(os.path.realpath(__file__))
 platforms_supported = ["x86_64-apple-darwin", "x86_64-pc-linux-gnu", "windows_x86_64"]
 
 def test():
-    #server_details_file = "server_auth_files/server_details"
-    server_details_file = "../test_auth/server_details"
+    server_details_file = "server_auth_files/server_details"
+    #server_details_file = "../test_auth/server_details"
     os.system("python ../create_app/create_app_wrapper.py {} {} --platforms_supported {}".format('cfg/app_config.json', server_details_file, ' '.join(platforms_supported)))
     #os.system("python ../create_app/create_app.py {} --platforms_supported {}".format('cfg/app_config.json', ' '.join(platforms_supported)))
 
